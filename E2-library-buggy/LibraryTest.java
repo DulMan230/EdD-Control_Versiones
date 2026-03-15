a9f09be95204c91dfdd8a9c68e7c2bdadeebf484
DulMan230 <danisanzf2006@gmail.com>
2026-03-16 00:46:24 +0100

    test: añadir pruebas para detectar libro duplicado

---
+++ E2-library-buggy/LibraryTest.java
@@ -0,0 +1,12 @@
+public class LibraryTest {
+
+    public void testAddDuplicateBook() {
+        Library library = new Library();
+
+        Book book1 = new Book("Test Book 1", "Author 1", "ISBN-123");
+        Book book2 = new Book("Test Book 2", "Author 2", "ISBN-123");
+
+        library.addBook(book1);
+        library.addBook(book2);
+    }
+}
